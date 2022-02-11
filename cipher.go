a010de95dd1075e8579068917c3c563f4ac1f61a
jjcapellan <soycape@hotmail.com>
2022-02-11 21:08:55 +0100

    New feature: Decrypt

--- cipher.go
+++ cipher.go
@@ -59,3 +59,56 @@ func Encrypt(src string, dst string, password string) error {
 
 	return nil
 }
+
+func Decrypt(src string, dst string, password string) error {
+	buffer, err := readFile(src)
+	if err != nil {
+		return err
+	}
+
+	// Gets 32 bytes long key from password
+	h := sha256.New()
+	h.Write([]byte(password))
+	key := h.Sum(nil)
+
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return err
+	}
+
+	s := gcm.NonceSize()
+
+	nonce, cipherContent := buffer[:s], buffer[s:]
+
+	content, err := gcm.Open(nil, nonce, cipherContent, nil)
+	if err != nil {
+		return err
+	}
+
+	dir, fName := filepath.Split(dst)
+	if dst == "" {
+		_, fName = filepath.Split(src)
+		fName = fName[:(len(fName) - 4)]
+		dst = fName
+	} else {
+		dst = filepath.Join(dir, fName)
+	}
+	if dir != "" {
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
+			os.MkdirAll(dir, 0777)
+		}
+	}
+
+	err = os.WriteFile(dst, content, 0777)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
--- cipher_test.go
+++ cipher_test.go
@@ -1,6 +1,24 @@
 package archiver
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
+func TestDecrypt(t *testing.T) {
+	src := "testmodels/packed2.tar.gz.crp"
+	dst := ""
+	password := "axdcf"
+	defer os.Remove("packed2.tar.gz")
+
+	err := Decrypt(src, dst, password)
+	if err != nil {
+		t.Fatalf("Decryption error: %s", err.Error())
+	}
+	if !compareFiles("testmodels/packed.tar.gz", "packed2.tar.gz") {
+		t.Fatalf("Not valid decrypted file")
+	}
+}
 
 func TestEncrypt(t *testing.T) {
 	src := "testmodels/packed.tar.gz"
